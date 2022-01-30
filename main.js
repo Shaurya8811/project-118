ddaf740e342a8b2e46c93e12f2ab6e91535d6e48
Shaurya8811 <78348328+Shaurya8811@users.noreply.github.com>
2022-01-30 21:13:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function preload()
+{
+
+
+}
+
+
+function setup()
+{
+canvas=createCanvas(280,280);
+canvas.center();
+background("white");
+}
+
+function clearcanvas()
+{
+background("white");
+}
+
